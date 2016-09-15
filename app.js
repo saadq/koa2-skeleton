7fa4780d8093c0e3150390dfbbbb0347c6a1e682
Saad Quadri <saad@saadquadri.com>
2016-09-15 00:49:29 -0400

    Add allowedMethods()

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const app = new Koa()
 app.use(views(`${__dirname}/views`, { extension: 'jade' }))
 app.use(serve(`${__dirname}/public`))
 app.use(rootRoutes.routes())
+app.use(rootRoutes.allowedMethods())
 app.use(userRoutes.routes())
+app.use(userRoutes.allowedMethods())
 
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000')
